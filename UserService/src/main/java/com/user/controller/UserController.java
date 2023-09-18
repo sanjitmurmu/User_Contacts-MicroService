cfa42ca7cadb9d4d4c8c41e1f4e8a0681284ec22
murmu <91903@LAPTOP-O8NT12LD>
2023-09-18 19:54:38 +0530

    Initial commit

--- UserService/src/main/java/com/user/controller/UserController.java
+++ UserService/src/main/java/com/user/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.user.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.user.entity.Contact;
 import com.user.entity.User;
 import com.user.service.UserService;
 
@@ -16,9 +20,16 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping("/{userId}")
 	public User getUser(@PathVariable("userId") Long userId) {
-		return userService.getUser(userId);
+		User user = userService.getUser(userId);
+//		http://localhost:9002/contact/user/1521
+		List<Contact> contacts = restTemplate.getForObject("http://contact-service/contact/user/"+user.getId(), List.class);
+		user.setContacts(contacts);
+		return user;
 	}
 
 }
